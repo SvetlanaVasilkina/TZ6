7804f08b0dfc567b2a384e36f118a9df2f74d261
SvetlanaVasilkina <103033438+SvetlanaVasilkina@users.noreply.github.com>
2022-06-22 07:22:00 +0300

    fix: добавить equals и hashcode

--- src/task/Subtask.java
+++ src/task/Subtask.java
@@ -23,4 +23,18 @@ public class Subtask extends Task {
                 ", taskId=" + getId() +
                 '}';
     }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Subtask subtask = (Subtask) o;
+        return epic.equals(subtask.epic);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), epic);
+    }
 }
